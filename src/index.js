cf731c21b16d866e537b0bdb62c0ca02ee5fbfc1
Cesare12 <591576872@qq.com>
2020-10-07 22:58:19 +0800

    static version

--- src/index.js
+++ src/index.js
@@ -5,8 +5,7 @@ import './index.css';
 class Display extends React.Component {
   render() {
     return (
-      
-      <input></input>
+      <div className='Display'>0</div>
     )
   }
 }
@@ -15,56 +14,62 @@ class Key extends React.Component {
   // constructor(props){
   //   super(props)
   // }
-  
-  
   render() {
-    console.log(this.props);
+    const keyValue = this.props.keyValue
     return (
-        <button/>
+      <button className={keyValue.attr} id={`key-${keyValue.char}`}>{keyValue.char}</button>
     )
   }
 }
+class KeyBoard extends React.Component {
+  render() {
+    const rows = [];
+    this.props.keyValues.forEach(k => {
+      rows.push(
+        <Key
+          keyValue={k}
+          key={k.char}
+        />
+      );
+    });
 
+    return (
+      <div className='KeyBoard'>{rows}</div>
+    )
+  }
+}
 
 class App extends React.Component {
   render() {
     return (
-      <>
-        <div>
-          <Display />
-        </div>
-        <div>
-          <Key value = {'AC'}/>
-          <Key>+</Key>
-          <Key />
-          <Key />
-        </div>
-        <div>
-          <Key />
-          <Key />
-          <Key />
-          <Key />
-        </div>
-        <div>
-          <Key />
-          <Key />
-          <Key />
-          <Key />
-        </div>
-        <div>
-          <Key />
-          <Key />
-          <Key />
-          <Key />
-        </div>
-        <div>
-          <Key />
-          <Key />
-        </div>
-      </>
+      <div className='app'>
+        <Display />
+        <KeyBoard keyValues={this.props.keyValues} />
+      </div>
     )
   }
 }
 
+const KEYBOARD = [
+  { attr: 'function', char: 'C' },
+  { attr: 'function', char: '±' },
+  { attr: 'function', char: '%' },
+  { attr: 'operator', char: '÷' },
+  { attr: 'digit', char: '7' },
+  { attr: 'digit', char: '8' },
+  { attr: 'digit', char: '9' },
+  { attr: 'operator', char: '×' },
+  { attr: 'digit', char: '4' },
+  { attr: 'digit', char: '5' },
+  { attr: 'digit', char: '6' },
+  { attr: 'operator', char: '-' },
+  { attr: 'digit', char: '1' },
+  { attr: 'digit', char: '2' },
+  { attr: 'digit', char: '3' },
+  { attr: 'operator', char: '+' },
+  { attr: 'digit', char: '0' },
+  { attr: 'digit', char: '.' },
+  { attr: 'operator', char: '=' }
+]
 const rootElement = document.getElementById('root')
-ReactDOM.render(<App />, rootElement)
+ReactDOM.render(<App keyValues={KEYBOARD} />, rootElement)
